dc0f543dfadef0345c7b95edf87e51440206ea71
Laureano55 <llaureano@uninorte.edu.co>
2025-10-28 21:29:38 -0500

    added CRUD and adjusted schema for unique licenses

--- src/infrastructure/controllers/conductores.controller.ts
+++ src/infrastructure/controllers/conductores.controller.ts
@@ -1,4 +1,32 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Param, Body } from '@nestjs/common';
+import { ConductoresService } from '../../services/conductores.service';
 
 @Controller('conductores')
-export class ConductoresController {}
+export class ConductoresController {
+	constructor(private readonly conductoresService: ConductoresService) {}
+
+	@Get()
+	async findAll() {
+		return this.conductoresService.findAll();
+	}
+
+	@Get(':id')
+	async findOne(@Param('id') id: string) {
+		return this.conductoresService.findOne(id);
+	}
+
+	@Post()
+	async create(@Body() data: any) {
+		return this.conductoresService.create(data);
+	}
+
+	@Put(':id')
+	async update(@Param('id') id: string, @Body() data: any) {
+		return this.conductoresService.update(id, data);
+	}
+
+	@Delete(':id')
+	async remove(@Param('id') id: string) {
+		return this.conductoresService.remove(id);
+	}
+}
--- src/services/conductores.service.ts
+++ src/services/conductores.service.ts
@@ -1,4 +1,33 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { PrismaService } from '../../prisma/prisma.service';
 
 @Injectable()
-export class ConductoresService {}
+export class ConductoresService {
+	constructor(private readonly prisma: PrismaService) {}
+
+	async findAll() {
+		return this.prisma.conductores.findMany();
+	}
+
+	async findOne(id: string) {
+		const conductor = await this.prisma.conductores.findUnique({
+			where: { id_usuario: id },
+		});
+		if (!conductor) throw new NotFoundException(`Conductor ${id} no encontrado`);
+		return conductor;
+	}
+
+	async create(data: any) {
+		return this.prisma.conductores.create({ data });
+	}
+
+	async update(id: string, data: any) {
+		await this.findOne(id);
+		return this.prisma.conductores.update({ where: { id_usuario: id }, data });
+	}
+
+	async remove(id: string) {
+		await this.findOne(id);
+		return this.prisma.conductores.delete({ where: { id_usuario: id } });
+	}
+}
