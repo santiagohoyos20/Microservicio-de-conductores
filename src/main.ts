14ac1df962740ea75494730a6a44b10e3e557ef6
Laureano55 <llaureano@uninorte.edu.co>
2025-10-29 05:17:40 -0500

    sw

--- src/main.ts
+++ src/main.ts
@@ -16,7 +16,7 @@ async function bootstrap() {
       .build();
 
     const document = SwaggerModule.createDocument(app, config);
-    SwaggerModule.setup('api', app, document);
+    SwaggerModule.setup('/conductores/api', app, document);
   }
 
   // Conectar microservicio gRPC
