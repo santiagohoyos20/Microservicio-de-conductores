f54f7c829f6d8dfca728bc0e04e45f286fa4783f
Laureano55 <llaureano@uninorte.edu.co>
2025-10-28 21:02:25 -0500

    added database local

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConductoresModule } from './infrastructure/modules/conductores.module';
-import { ConductoresController } from './infrastructure/controllers/conductores.controller';
-import { ConductoresService } from './services/conductores.service';
 
 @Module({
   imports: [ConductoresModule],
-  controllers: [AppController, ConductoresController],
-  providers: [AppService, ConductoresService],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
--- src/infrastructure/modules/conductores.module.ts
+++ src/infrastructure/modules/conductores.module.ts
@@ -1,4 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ConductoresController } from '../controllers/conductores.controller';
+import { ConductoresService } from '../../services/conductores.service';
+import { PrismaModule } from '../../../prisma/prisma.module';
 
-@Module({})
+@Module({
+	imports: [PrismaModule],
+	controllers: [ConductoresController],
+	providers: [ConductoresService],
+	exports: [ConductoresService],
+})
 export class ConductoresModule {}
